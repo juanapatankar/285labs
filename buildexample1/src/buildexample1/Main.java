714615f21815a1ab648a75e18230992a0962031c
Juana Patankar <juanapatankar4@gmail.com>
2022-02-02 21:39:32 +0000

    done lab 1

---
+++ buildexample1/src/buildexample1/Main.java
@@ -0,0 +1,13 @@
+package buildexample1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=0; i<args.length; i++) 
+		{
+			System.out.println(args[i]);
+		}
+	}
+
+}
---
+++ proj-joint-source-classes/mystuff/labs/hello/HelloWorld2.java
@@ -0,0 +1,13 @@
+package mystuff.labs.hello;
+
+public class HelloWorld2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=0; i<34; i++) 
+		{
+			System.out.println("honk");
+		}
+	}
+
+}
